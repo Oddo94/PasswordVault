7caded6d7da742a4bf560c2aaea2ac7153fdfc05
Oddo94 <57127779+Oddo94@users.noreply.github.com>
2024-10-23 18:53:50 +0300

    Record editing system refactor (#5)
    Adds the improved record editing system to the master branch

--- src/com/razvan/gui/UserTableOperations.java
+++ src/com/razvan/gui/UserTableOperations.java
@@ -4,6 +4,8 @@ import com.razvan.installation_manager.ApplicationInstallManager;
 import com.razvan.io_manager.IOFileManager;
 import com.razvan.user_data_security.UserDataSecurityManager;
 import com.razvan.utils.GUIInputChecker;
+import com.razvan.utils.events.EditEventListener;
+import com.razvan.utils.model.AccountRecord;
 
 import javax.crypto.SecretKey;
 import javax.swing.*;
@@ -26,6 +28,7 @@ public class UserTableOperations extends MouseAdapter {
 	private UserDashboard userDashboard;
 	private JTable userDataTable;
 	static int userOption;
+	private static boolean hasRequestedRowEdit;
 	private boolean hasCanceledChange;
 	private boolean hasCopiedCellContent;
 	private String oldCellValue;
@@ -33,6 +36,7 @@ public class UserTableOperations extends MouseAdapter {
 	private String userFileName;
 	private IOFileManager fileManager = new IOFileManager();
 	private UserDataSecurityManager securityManager = new UserDataSecurityManager();
+	private EditEventListener editEventListener;
 
 	public UserTableOperations(UserDashboard userDashboard, String userFileName) {
 		this.userDashboard = userDashboard;
@@ -80,7 +84,7 @@ public class UserTableOperations extends MouseAdapter {
 
 
 	/*
-	 * The method that creates the table that displays the user data. 
+	 * The method that creates the table that displays the user data.
 	 * It receives the number of rows and columns as parameters.
 	 * This data also used for creating the DefaultTableModel.
 	 */
@@ -90,11 +94,20 @@ public class UserTableOperations extends MouseAdapter {
 		//Creates the table
 		userDataTable = new JTable(numRows,numColumns);
 		
+		// //Sets the table cell renderer in order to allow the highlighting of expired passwords
+		// setTableCellRendererForExpiredPasswordHighlight(userDataTable);
+
 		//Sets the table cell renderer in order to allow the highlighting of expired passwords
 		setTableCellRendererForExpiredPasswordHighlight(userDataTable);
 
-		//Creates the table model
-		DefaultTableModel tableModel = new DefaultTableModel(numRows,numColumns);
+		//Creates the table model and disables cell editing (isCellEditable method will return false for any cell)
+		DefaultTableModel tableModel = new DefaultTableModel(numRows,numColumns) {
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+		};
+
 		tableModel.setColumnIdentifiers(columnNames);
 		userDataTable.setModel(tableModel);
 		//Enables the sorting option on the columns of the table
@@ -112,7 +125,6 @@ public class UserTableOperations extends MouseAdapter {
 		JScrollPane tableScrollPane = new JScrollPane(userDataTable);
 		tablePanel.add(tableScrollPane);
 
-		
 		//Adds the table panel to the user dashboard
 		userDashboard.add(tablePanel, new BorderLayout().CENTER);
 
@@ -148,7 +160,7 @@ public class UserTableOperations extends MouseAdapter {
 		File userDataFile = new File(appDataPath + "/userData/Razvan");
 
 		//Secret key retrieval and encryption of the table data	
-		SecretKey secretKey = securityManager.retrieveSecretKey(userFileName);	
+		SecretKey secretKey = securityManager.retrieveSecretKey(userFileName);
 		byte[] encryptedData = securityManager.performAESEncryption(tableData, secretKey, initializationVector);
 
 		//Null check to make sure the encryption process was successful
@@ -234,16 +246,19 @@ public class UserTableOperations extends MouseAdapter {
 
 	/*
 	 * The method used for adding a MouseListener to the table so that when the user
-	 * right clicks the mouse a menu containing the options "Copy" and "Delete" will
+	 * right clicks the mouse, a menu containing the options "Copy" and "Delete" will
 	 * appear
 	 */
 	public void addMouseListenerToTable() {
 		JPopupMenu popupMenu = new JPopupMenu();
 
-		JMenuItem deleteRowOption = new JMenuItem("Delete");
 		JMenuItem copyCellDataOption = new JMenuItem("Copy");
+		JMenuItem editRowOption = new JMenuItem("Edit");
+		JMenuItem deleteRowOption = new JMenuItem("Delete");
+
 
 		popupMenu.add(copyCellDataOption);
+		popupMenu.add(editRowOption);
 		popupMenu.add(deleteRowOption);
 
 		userDataTable.addMouseListener(new MouseAdapter(){
@@ -266,8 +281,9 @@ public class UserTableOperations extends MouseAdapter {
 			}
 		});
 
-		addMouseListenerToDeleteOption(deleteRowOption, userDataTable.getSelectedRow());
 		addMouseListenerToCopyOption(copyCellDataOption, userDataTable.getSelectedRow());
+		addMouseListenerToEditOption(editRowOption, userDataTable.getSelectedRow());
+		addMouseListenerToDeleteOption(deleteRowOption, userDataTable.getSelectedRow());
 	}
 
 	//Method for adding a TableModelListener which is responsible for managing the changes performed to the table data
@@ -279,71 +295,54 @@ public class UserTableOperations extends MouseAdapter {
 
 				if (e.getType() == TableModelEvent.UPDATE && !hasCopiedCellContent) {
 
-					if (hasCanceledChange) {
-						hasCanceledChange = false;
-						return;
-					}
-
-					DefaultTableModel dtm = (DefaultTableModel) userDataTable.getModel();
-
-
-					userOption = JOptionPane.showConfirmDialog(null,"Do you want to save the changes?","Data saving",JOptionPane.YES_NO_OPTION);
-
-					//Checks if the modified date is correct(from the value perspective) and has the right format after the user has changed it
-					String modifiedDate =(String) dtm.getValueAt(e.getFirstRow(), e.getColumn());
-					if(!GUIInputChecker.isValidDate(modifiedDate, "dd-MM-yyyy")) {
-						JOptionPane.showMessageDialog(userDashboard, "Invalid date and/or format! The date must have the format 'dd-mm-yyyy'.", "User dashboard", JOptionPane.WARNING_MESSAGE);
-						//Sets the flag to true as the change will not be saved due to the incorrect date format
-						hasCanceledChange = true;
-						//Sets the cell content to its old value since the user provided data is incorrect
-						dtm.setValueAt(oldCellValue, e.getFirstRow(), e.getColumn());
-						return;
-					}
-
-
-					if (userOption == 0) {
-						editEntry();
-					} else {
-						hasCanceledChange = true;
-						System.out.println("NO option selected");
-						dtm.setValueAt(oldCellValue, e.getFirstRow(), e.getColumn());
-						System.out.println("Changed row :" +  e.getFirstRow() +"\n" + "Changed column:" + e.getColumn());;
-					}
-
-					System.out.println("Table was updated!");
+//					if (hasCanceledChange) {
+//						hasCanceledChange = false;
+//						return;
+//					}
+//
+//					DefaultTableModel dtm = (DefaultTableModel) userDataTable.getModel();
+//
+//
+//					userOption = JOptionPane.showConfirmDialog(null,"Do you want to save the changes?","Data saving",JOptionPane.YES_NO_OPTION);
+//
+//					//Checks if the modified date is correct(from the value perspective) and has the right format after the user has changed it
+//					String modifiedDate =(String) dtm.getValueAt(e.getFirstRow(), e.getColumn());
+//					if(!GUIInputChecker.isValidDate(modifiedDate, "dd-MM-yyyy")) {
+//						JOptionPane.showMessageDialog(userDashboard, "Invalid date and/or format! The date must have the format 'dd-mm-yyyy'.", "User dashboard", JOptionPane.WARNING_MESSAGE);
+//						//Sets the flag to true as the change will not be saved due to the incorrect date format
+//						hasCanceledChange = true;
+//						//Sets the cell content to its old value since the user provided data is incorrect
+//						dtm.setValueAt(oldCellValue, e.getFirstRow(), e.getColumn());
+//						return;
+//					}
+//
+//
+//					if (userOption == 0) {
+//						editEntry();
+//					} else {
+//						hasCanceledChange = true;
+//						System.out.println("NO option selected");
+//						dtm.setValueAt(oldCellValue, e.getFirstRow(), e.getColumn());
+//						System.out.println("Changed row :" +  e.getFirstRow() +"\n" + "Changed column:" + e.getColumn());;
+//					}
+//
+//					System.out.println("Table was updated!");
+					//editEntry();
 
 				} else if (e.getType() == TableModelEvent.INSERT) {
 
 					if (userOption == 0) {
 						//System.out.println("User option for insertion " + userOption);
-						addNewEntry(getTableData());	
+						addNewEntry(getTableData());
 					}
 
 				} else if (e.getType() == TableModelEvent.DELETE) {
 					removeEntry(getTableData());
-				}	
+				}
 			}
-		};	
+		};
 
 		dtm.addTableModelListener(tableListener);
-	}
-
-	//Adding MouseListener for the delete option displayed when right clicking on a table cell
-	public void addMouseListenerToDeleteOption(JMenuItem popupItem, int entryNumber) {
-
-		popupItem.addMouseListener(new MouseAdapter() {
-			public void mousePressed(MouseEvent e) {				
-				//removeEntry(getSelectedRowData());
-				DefaultTableModel dtm = (DefaultTableModel) userDataTable.getModel();
-
-				userOption = JOptionPane.showConfirmDialog(null, "Are you sure that you want to delete the selected row?", "Delete confirmation", JOptionPane.YES_NO_OPTION);
-
-				if (userOption == 0) {
-					dtm.removeRow(userDataTable.getSelectedRow());
-				}
-
-			}
-		});		
 	}
 
 	//Adding MouseListener for the copy option displayed when right clicking on a table cell
@@ -363,9 +362,43 @@ public class UserTableOperations extends MouseAdapter {
 			}
 		});
 	}
+	public void addMouseListenerToEditOption(JMenuItem popupItem, int entryNumber) {
+		popupItem.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+//				userOption = JOptionPane.showConfirmDialog(null, "Are you sure that you want to edit the selected record?", "Edit confirmation", JOptionPane.YES_NO_OPTION);
+//
+//				if(userOption == 0) {
+//
+//				}
+				if (editEventListener != null) {
+					AccountRecord accountRecord = getSelectedAccountRecord();
+					editEventListener.onEdit(accountRecord);
+				}
+
+			}
+		});
+	}
+
+	//Adding MouseListener for the delete option displayed when right clicking on a table cell
+	public void addMouseListenerToDeleteOption(JMenuItem popupItem, int entryNumber) {
+
+		popupItem.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				//removeEntry(getSelectedRowData());
+				DefaultTableModel dtm = (DefaultTableModel) userDataTable.getModel();
+
+				userOption = JOptionPane.showConfirmDialog(null, "Are you sure that you want to delete the selected row?", "Delete confirmation", JOptionPane.YES_NO_OPTION);
+
+				if (userOption == 0) {
+					dtm.removeRow(userDataTable.getSelectedRow());
+				}
+
+			}
+		});		
+	}
 
 	/*
-	 * Method used for retrieving table data. The string stores the data in the following format: 
+	 * Method used for retrieving table data. The string stores the data in the following format:
 	 * -the cell data contained in a row is separated by a ","
 	 * -each row is separated by a newline ("\n") character
 	 */
@@ -391,71 +424,126 @@ public class UserTableOperations extends MouseAdapter {
 	}
 
 	//The method that returns the values from the selected row
-	public String getSelectedRowData() {
-		StringBuilder sb = new StringBuilder();
+//	public String getSelectedRowData() {
+//		StringBuilder sb = new StringBuilder();
+//		int row = userDataTable.getSelectedRow();
+//		int columnCount = userDataTable.getColumnCount();
+//
+//		for (int i = 0; i < columnCount; i++) {
+//			if (i == columnCount - 1) {
+//				sb.append(userDataTable.getValueAt(row, i));
+//				break;
+//			}
+//			sb.append(userDataTable.getValueAt(row, i) + "||");
+//		}
+//
+//		return sb.toString();
+//	}
+
+	public AccountRecord getSelectedAccountRecord() {
 		int row = userDataTable.getSelectedRow();
+
+		int accountNameColumnIndex = 0;
+		int usernameColumnIndex= 1;
+		int passwordColumnIndex = 2;
+		int lastChangeDateColumnIndex = 3;
+
+		AccountRecord selectedAccount = new AccountRecord();
+		selectedAccount.setAccountName(userDataTable.getValueAt(row, accountNameColumnIndex).toString());
+		selectedAccount.setUsername(userDataTable.getValueAt(row, usernameColumnIndex).toString());
+		selectedAccount.setPassword(userDataTable.getValueAt(row, passwordColumnIndex).toString());
+		selectedAccount.setLastChangeDate(userDataTable.getValueAt(row, lastChangeDateColumnIndex).toString());
+
+		return selectedAccount;
+	}
+
+
+	//Method used for retrieving the row index based on the content of the selected record
+	public int getSelectedRowIndexBasedOnRecord(AccountRecord accountRecord) {
+		Objects.requireNonNull(accountRecord, "The provided account record must not be null!");
+
+		int rowCount = userDataTable.getRowCount();
 		int columnCount = userDataTable.getColumnCount();
 
-		for (int i = 0; i < columnCount; i++) {
-			if (i == columnCount - 1) {
-				sb.append(userDataTable.getValueAt(row, i));
-				break;
+		for (int i = 0; i < rowCount; i++) {
+			String accountName = userDataTable.getValueAt(i, 0).toString();
+			String userName = userDataTable.getValueAt(i, 1).toString();
+			String password = userDataTable.getValueAt(i, 2).toString();
+			String lastChangeDate = userDataTable.getValueAt(i, 3).toString();
+
+			if (accountName.equals(accountRecord.getAccountName())
+					&& userName.equals(accountRecord.getUsername())
+					&& password.equals(accountRecord.getPassword())
+					&& lastChangeDate.equals(accountRecord.getLastChangeDate())
+			) {
+				return i;
 			}
-			sb.append(userDataTable.getValueAt(row, i) + ",");
 		}
 
-
-		return sb.toString();
+		return -1;
 	}
+
 
 	//Sets the table cell renderer so that the expired passwords are highlighted
 	private void setTableCellRendererForExpiredPasswordHighlight(JTable table) {
 		Objects.requireNonNull(table, "The table object provided for setting the cell renderer cannot be null.");
-		
+
 		table.setDefaultRenderer(Object.class, new DefaultTableCellRenderer(){
-		    @Override
-		    public Component getTableCellRendererComponent(JTable table,
-		            Object value, boolean isSelected, boolean hasFocus, int row, int col) {
+			@Override
+			public Component getTableCellRendererComponent(JTable table,
+														   Object value, boolean isSelected, boolean hasFocus, int row, int col) {
 
-		        super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, col);
+				super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, col);
 
-		        String status = (String)table.getModel().getValueAt(row, 3);
+				String status = (String)table.getModel().getValueAt(row, 3);
 				DefaultTableModel dtm = getUserDataTableModel();
 				String passwordDateFormat = "dd-MM-yyyy";
-				
-				
+
+
 				int passwordDateColumnIndex = 3;
 				//The number of days from creation after which the password is considered expired
 				int daysSinceCreationLimit = 180;
 
 
 				String passwordDate = (String) dtm.getValueAt(row, passwordDateColumnIndex);
-					boolean isExpiredPassword = false;
-					boolean wasChecked = false;
+				boolean isExpiredPassword = false;
+				boolean wasChecked = false;
 
-					try {
-						isExpiredPassword = GUIInputChecker.isExpiredPassword(passwordDate, passwordDateFormat, daysSinceCreationLimit);
-						wasChecked = true;
+				try {
+					isExpiredPassword = GUIInputChecker.isExpiredPassword(passwordDate, passwordDateFormat, daysSinceCreationLimit);
+					wasChecked = true;
 
-					} catch (Exception ex) {
-						ex.printStackTrace();
-					}
-		        		           
-		        
-		        if (isExpiredPassword && wasChecked) {
-		            //Sets the RED/WHITE color scheme for highlighting expired passwords
-		        	setBackground(Color.RED);
-		            setForeground(Color.WHITE);
-		        } else {
-		        	//Sets WHITE/BLACK standard color scheme if the password is not expired
-		            setBackground(table.getBackground());
-		            setForeground(table.getForeground());
-		        }       
-		        return this;
-		    }   
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+
+
+				if (isExpiredPassword && wasChecked) {
+					//Sets the RED/WHITE color scheme for highlighting expired passwords
+					setBackground(Color.RED);
+					setForeground(Color.WHITE);
+				} else {
+					//Sets WHITE/BLACK standard color scheme if the password is not expired
+					setBackground(table.getBackground());
+					setForeground(table.getForeground());
+				}
+				return this;
+			}
 		});
 	}
-	
+
+	public int updateSelectedRow(AccountRecord accountRecord, int selectedRowIndex) {
+		if (accountRecord == null) {
+			return -1;
+		}
+
+		userDataTable.setValueAt(accountRecord.getAccountName(), selectedRowIndex, 0);
+		userDataTable.setValueAt(accountRecord.getUsername(),selectedRowIndex, 1);
+		userDataTable.setValueAt(accountRecord.getPassword(),selectedRowIndex, 2);
+		userDataTable.setValueAt(accountRecord.getLastChangeDate(),selectedRowIndex, 3);
+
+		return 0;
+	}
 
 
 	public DefaultTableModel getUserDataTableModel() {
@@ -464,6 +552,18 @@ public class UserTableOperations extends MouseAdapter {
 
 	public JTable getUserDataTable() {
 		return this.userDataTable;
+	}
+
+	public boolean getHasRequestedRowEdit() {
+		return hasRequestedRowEdit;
+	}
+
+	public void setHasRequestedRowEdit(boolean state) {
+		hasRequestedRowEdit = state;
+	}
+
+	public void setEditEventListener(EditEventListener editEventListener) {
+		this.editEventListener = editEventListener;
 	}
 }
 
---
+++ src/com/razvan/utils/events/EditEventListener.java
@@ -0,0 +1,7 @@
+package com.razvan.utils.events;
+
+import com.razvan.utils.model.AccountRecord;
+
+public interface EditEventListener {
+    void onEdit(AccountRecord accountRecord);
+}
